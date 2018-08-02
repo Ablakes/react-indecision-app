17e168582fee5c322e4f3ee4939bfc828a00cc43
Ablakes <blakesley.alex@gmail.com>
2018-08-02 14:12:38 -0700

    clean up comments

--- src/app.js
+++ src/app.js
@@ -7,20 +7,3 @@ import "normalize-css/normalize.css";
 import "./styles/styles.scss";
 
 ReactDOM.render(<IndecisionApp />, document.getElementById("app"));
-
-// const Layout = props => {
-
-// ReactDOM.render(
-//   <Layout>
-//     <div>
-//       <h2>moar</h2>
-//     </div>
-//   </Layout>,
-//   document.getElementById("app")
-// );
-
-// class NewSyntax {
-//   name = "alex";
-// }
-// const newSyntax = new NewSyntax();
-// console.log(newSyntax);
